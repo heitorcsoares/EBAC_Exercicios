0adadfbb331a28c6f01e2694a1bc8674c5bc7957
Heitor Soares <heitor.c.soares@gmail.com>
2023-03-01 15:55:58 -0300

    commit

--- Modulo9_Layouts_Exercicios/app/src/main/java/com/example/modulo9_layouts_exercicios/MainActivity.kt
+++ Modulo9_Layouts_Exercicios/app/src/main/java/com/example/modulo9_layouts_exercicios/MainActivity.kt
@@ -6,36 +6,36 @@ import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var celular: TextView? = null
-    var email: TextView? = null
-    var endereco: TextView? = null
-    var github: TextView? = null
-    var instagram: TextView? = null
+    private var celular: TextView? = null
+    private var email: TextView? = null
+    private var endereco: TextView? = null
+    private var github: TextView? = null
+    private var instagram: TextView? = null
 
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        celular = findViewById<TextView>(R.id.textCelular)
-        email = findViewById<TextView>(R.id.textEmail)
-        endereco = findViewById<TextView>(R.id.textEndereco)
-        github = findViewById<TextView>(R.id.textGithub)
-        instagram = findViewById<TextView>(R.id.textInstagram)
+        celular = findViewById(R.id.textCelular)
+        email = findViewById(R.id.textEmail)
+        endereco = findViewById(R.id.textEndereco)
+        github = findViewById(R.id.textGithub)
+        instagram = findViewById(R.id.textInstagram)
 
         val destaque = findViewById<Switch>(R.id.switch1)
 
         destaque.isChecked = false
 
-        destaque.setOnCheckedChangeListener { buttonView, isChecked ->
+        destaque.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                EditarEstilo(R.style.texto2)
+                editarEstilo(R.style.texto2)
             } else {
-                EditarEstilo(R.style.texto)
+                editarEstilo(R.style.texto)
             }
         }
     }
 
-    fun EditarEstilo (estilo: Int){
+    fun editarEstilo (estilo: Int){
         celular?.setTextAppearance(estilo)
         email?.setTextAppearance(estilo)
         endereco?.setTextAppearance(estilo)
