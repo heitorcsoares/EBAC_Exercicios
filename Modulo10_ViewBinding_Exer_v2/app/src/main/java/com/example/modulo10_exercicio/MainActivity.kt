d3602885e96fecb1fa97d0e1ff196c9535aebcf5
Heitor Soares <heitor.c.soares@gmail.com>
2023-04-10 16:25:03 -0300

    Correção e Melhorias
    - Feita atualização do RecyclerView.
    - Melhorias no local de declaração e inicialização de variais para recuperação de dados.

--- Modulo10_ViewBinding_Exer_v2/app/src/main/java/com/example/modulo10_exercicio/MainActivity.kt
+++ Modulo10_ViewBinding_Exer_v2/app/src/main/java/com/example/modulo10_exercicio/MainActivity.kt
@@ -14,6 +14,11 @@ class MainActivity : AppCompatActivity(), ListAdapter.ListAdapterListener {
 
     private lateinit var binding: ActivityMainBinding
     private val itemList: MutableList<ListItem> = mutableListOf()
+    private lateinit var listAdapter: ListAdapter
+
+    var editName: EditText? = null
+    var editBirth: EditText? = null
+    var editPhonee: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +29,12 @@ class MainActivity : AppCompatActivity(), ListAdapter.ListAdapterListener {
         val listView = binding.lista
         val button = findViewById<Button>(R.id.button)                                   //Variavel Recebe Botão
 
+        iniciarEdit()
         createMockDate()                                                                //Chama função para: Povoa dados na lista
 
         listView.layoutManager = LinearLayoutManager(this)
-        listView.adapter = ListAdapter(itemList, this)
+        listAdapter = ListAdapter(itemList, this)
+        listView.adapter = listAdapter
 
         button.setOnClickListener {                                              //evento de click no Botão Gravar
             adicionar()                                                                 //chama função para incluir dados na lista
@@ -39,19 +46,15 @@ class MainActivity : AppCompatActivity(), ListAdapter.ListAdapterListener {
     }
 
     private fun adicionar() {
-        var editName: EditText? = findViewById<EditText>(R.id.editName)
-        var editBirth: EditText? = findViewById<EditText>(R.id.editBirth)
-        var editPhonee: EditText? = findViewById<EditText>(R.id.editPhonee)
-
         var name: String = editName?.text.toString()
         var birth: String = editBirth?.text.toString()
         var phonee: String = editPhonee?.text.toString()
 
         itemList.add(ListItem(name, birth, phonee))
-        //notifyDataSetChanged()
+
+        listAdapter.notifyDataSetChanged()
 
         val listUltimo = itemList.last().name
-
         Toast.makeText(this, "Contato: $listUltimo", Toast.LENGTH_SHORT).show()
 
         editName?.setText("")
@@ -59,8 +62,20 @@ class MainActivity : AppCompatActivity(), ListAdapter.ListAdapterListener {
         editPhonee?.setText("")
     }
 
-    private fun createMockDate(){                                                       //Adicioando Itens a lista(itemList).
-        itemList.add(ListItem("Heitor",     "01/01/2023",   "(21)99645-1569"))
-        itemList.add(ListItem("Luciano",    "01/01/2023",   "(21)99645-1569"))
+    private fun createMockDate(){                                                                    //Adicioando Itens a lista(itemList).
+        itemList.add(ListItem("Heitor",     "01/01/2023",   "(21)99991-1234"))
+        itemList.add(ListItem("Luciano",    "02/02/2023",   "(21)91234-5555"))
+        itemList.add(ListItem("Gabriel",    "01/01/2023",   "(21)99991-1234"))
+        itemList.add(ListItem("Henrique",   "02/02/2023",   "(21)91234-7788"))
+        itemList.add(ListItem("Pedro",      "05/01/2023",   "(21)99988-1234"))
+        itemList.add(ListItem("Medeiros",   "07/02/2023",   "(21)96666-5555"))
+        itemList.add(ListItem("Paulo",      "09/01/2023",   "(21)94455-1234"))
+        itemList.add(ListItem("Gustavo",    "12/02/2023",   "(21)91234-8887"))
+    }
+
+    private fun iniciarEdit(){
+        editName = findViewById<EditText>(R.id.editName)
+        editBirth = findViewById<EditText>(R.id.editBirth)
+        editPhonee = findViewById<EditText>(R.id.editPhonee)
     }
 }
