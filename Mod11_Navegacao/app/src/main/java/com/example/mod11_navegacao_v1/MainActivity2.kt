12c5a86fde19b19a3668e73a432c72194786bc11
Heitor Soares <heitor.c.soares@gmail.com>
2023-04-12 16:09:28 -0300

    Reescrito

---
+++ Mod11_Navegacao/app/src/main/java/com/example/mod11_navegacao_v1/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.mod11_navegacao_v1
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        if (intent?.extras?.isEmpty == false) {
+            val name = intent.getStringExtra("name")
+            val age = intent.getIntExtra("age",0)
+        }
+
+        val result = Intent()
+        result.putExtra("result","Navegação com intents")
+        setResult(Activity.RESULT_OK,result)
+        finish()
+    }
+}
