1295cf7e1f85616a4f6021ef2a97660751d804c8
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2023-08-01 12:32:54 -0300

    Exercicio correção
    Verificar se itens estão sendo feitos.
    - Ao girar o telefone, a tela não se move e o campo fica escondido.
    Como posso configurara para arrastar para cima?

---
+++ Mod13_Exercicio_Jokenpo/app/src/main/java/com/example/mod12_exercicio_jokenpo/jogadorFragment.kt
@@ -0,0 +1,86 @@
+package com.example.mod12_exercicio_jokenpo
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import com.example.mod12_exercicio_jokenpo.databinding.ActivityMainBinding
+import com.example.mod12_exercicio_jokenpo.databinding.FragmentJogadorBinding
+import com.google.android.material.snackbar.Snackbar
+
+class jogadorFragment : Fragment() {
+    lateinit var root: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("LifeCycle","onCreate - OK")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentJogadorBinding.inflate(inflater,container,false)
+        root = binding.root
+
+        setHasOptionsMenu(true)
+
+        Log.d("LifeCycle","onCreateView - OK")
+
+        return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.tela2_reiniciar_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController())
+    }
+
+
+    //**
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("LifeCycle","onResume - OK")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("LifeCycle","onStop - OK")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("LifeCycle","onDestroy - OK")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+        Log.d("LifeCycle","onDetach - OK")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        Log.d("LifeCycle","onAttach - OK")
+    }
+
+}
